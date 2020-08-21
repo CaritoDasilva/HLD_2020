0ace13ef12f07503d6ce76b5ddfd7fd2cd11fa63
Mariel Quezada <mariel@yapp.cl>
2020-08-21 19:25:04 -0400

    instalacion de bootstrap y creacion componente onboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OnboardComponent } from './pages/onboard/onboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OnboardComponent
   ],
   imports: [
     BrowserModule,
