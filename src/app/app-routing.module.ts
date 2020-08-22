09321ede8efcf8b09f44d9ede99ba0a38f3b92bc
Mariel Quezada <mariel@yapp.cl>
2020-08-21 20:18:50 -0400

    pantalla onboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { OnboardComponent } from './pages/onboard/onboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'onboard', component: OnboardComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
